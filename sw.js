0f02a468312cac233357e6b77b3389cdcab2faa2
Rootadmin0101 <ayatebadi@gmail.com>
2026-06-23 00:34:05 +0430

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,10 @@
-const CACHE_NAME = 'hesab-v8-cache';
+const CACHE_NAME = 'hesab-v5-cache';
 const ASSETS = [
   './index.html',
-  './manifest.json',
-  './icon.png'
+  './manifest.json'
 ];
 
-// مرحله نصب و ذخیره فایل‌ها در حافظه گوشی
 self.addEventListener('install', e => {
-  self.skipWaiting(); // اجبار به آپدیت فوری در صورت وجود نسخه جدید
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(ASSETS);
@@ -15,23 +12,6 @@ self.addEventListener('install', e => {
   );
 });
 
-// مرحله فعال‌سازی و پاک کردن کش‌های قدیمی
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key); // حذف نسخه‌های قبلی برنامه از حافظه
-          }
-        })
-      );
-    })
-  );
-  return self.clients.claim();
-});
-
-// مرحله فراخوانی فایل‌ها در حالت آفلاین
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(response => {
