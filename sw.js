24b7b8057ebb3de608ace619d213b2be6835c600
Rootadmin0101 <ayatebadi@gmail.com>
2026-06-25 10:39:28 +0430

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,36 @@
-const CACHE_NAME = 'hesab-v5-cache';
+const CACHE_NAME = 'hesab-app-v2';
 const ASSETS = [
+  './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon.png'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
+// نصب سرویس‌ورکر و ذخیره فایل‌ها در حافظه کش گوشی
+self.addEventListener('install', event => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(ASSETS);
     })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+// استفاده از فایل‌های ذخیره‌شده زمانی که اینترنت قطع است
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// پاک کردن حافظه کش قدیمی وقتی کدها را به‌روزرسانی می‌کنید
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
     })
   );
 });
